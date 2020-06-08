b134884fb8deb2517ade11ae1a38b0ad36b6ea16
Oluwatobii <bellotobi10@gmail.com>
2020-06-08 18:25:18 +0000

    Determines how many hundreds can be made from a number

---
+++ how-many-hundreds.js
@@ -0,0 +1,12 @@
+const howManyHundreds = function (number){
+  const containers = number / 100;
+  number = Math.floor(containers)
+  return number
+}
+
+console.log(howManyHundreds(1000));
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
