88bb5a183aad0b367697c09ab6ea61098200a2fa
Oluwatobii <bellotobi10@gmail.com>
2020-06-08 18:14:24 +0000

    Determins someone's age when given their date of birth

---
+++ age-calculator.js
@@ -0,0 +1,10 @@
+const ageCalculator = function (name, yearOfBirth, currentYear){
+  const currentAge = currentYear - yearOfBirth;
+  return name + " is " + currentAge + " years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+console.log(ageCalculator("Suzie", 1983, 2015));
+console.log(ageCalculator("Jack", 2003, 2015));
+console.log(ageCalculator("Ali", 2015, 2015));
