1a41ec60e47fb1d6603892c2c12720cde2b8c0e1
Oluwatobii <bellotobi10@gmail.com>
2020-06-08 00:47:39 +0000

    print arrays in  both forward  and reverse order

--- packing-list.js
+++ packing-list.js
@@ -1,3 +1,5 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
 console.log("Kitchen stuff to pack:");
 let i = 0;
 while (i < packingList.length) {
