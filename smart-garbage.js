fecbae8ec1fffb94f73f3dd0143d46bf331fd772
Oluwatobii <bellotobi10@gmail.com>
2020-06-17 00:37:34 +0000

    A function which will be responsible for increasing the garbage count for waste, recycling, or compost

--- smart-garbage.js
+++ smart-garbage.js
@@ -18,7 +18,7 @@ console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }));
 const smartGarbage = function (trash, bins){
 
   const keys = Object.keys(bins); // this lines extracts all the keys from bins into an array
-  console.log(keys);
+  //console.log(keys);
 
   // this is looping through the array of keys from bins
   for (let i=0; i< keys.length; i++){
