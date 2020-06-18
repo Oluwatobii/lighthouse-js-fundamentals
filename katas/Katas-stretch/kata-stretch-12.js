790973efde67131853f7d00599ab351a3aefd00d
Oluwatobii <bellotobi10@gmail.com>
2020-06-18 21:03:36 +0000

    Extra Questions

--- katas/Katas-stretch/kata-stretch-12.js
+++ katas/Katas-stretch/kata-stretch-12.js
@@ -1,22 +1,28 @@
 const organizeInstructors = function(instructors) {
   // Put your solution here
   newArrangement = {};
-  for (i = 0; i < instructors; i++){
-    let courses = instructors[i].course;
-    console.log(courses);
-    //let instructors = instructors[i].name;
+  for (obj of instructors){
+    let arrayOutput = [];
+    let newKey = obj.course;
+    for (secondObj of instructors){
+      if (newKey === secondObj.course){
+        arrayOutput.push(secondObj.name)
+      }
+    }
+    newArrangement[newKey] = arrayOutput //console.log (obj)
   }
+  return newArrangement
 };
 
-// console.log(organizeInstructors([
-//   {name: "Samuel", course: "iOS"},
-//   {name: "Victoria", course: "Web"},
-//   {name: "Karim", course: "Web"},
-//   {name: "Donald", course: "Web"}
-// ]));
-// console.log(organizeInstructors([
-//   {name: "Brendan", course: "Blockchain"},
-//   {name: "David", course: "Web"},
-//   {name: "Martha", course: "iOS"},
-//   {name: "Carlos", course: "Web"}
-// ]));
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
